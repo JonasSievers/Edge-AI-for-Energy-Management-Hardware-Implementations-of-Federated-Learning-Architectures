1d932a2bdebd8f96fcd2c0681b123d7aeb44369a
Yannick Fritsch <uoenn@student.kit.edu>
2024-04-18 11:41:38 +0200

    electricity_price implementation raspberry pi

--- src/Data collection/Raspberrypi Implementation/electricity_price_data_collection_CRON.py
+++ src/Data collection/Raspberrypi Implementation/electricity_price_data_collection_CRON.py
@@ -11,8 +11,10 @@
 #   |_| /_/(_) \___/    |_|(_)|____| \___/|____|   |_|                              
 
 
-from datetime import datetime
+from datetime import datetime, timedelta
+import re
 import pandas as pd
+import pytz
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -36,7 +38,7 @@ SMARD_RESOLUTION_TIMESTAMP = "hour"
 SMARD_RESOLUTION_ELECTRICITY = "quaterhourly"
 
 DATA_FOLDER = os.path.dirname(os.path.realpath(__file__))
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug(f"Data folder is {DATA_FOLDER}")
 
 DATA_COLLECTION_DAY = 10 #(1 is current day, 2 is today and yesterday)
@@ -51,6 +53,119 @@ REMOVE_OLD_DATA_AFTER_DAYS = 30
 current_time_seconds = current_datetime = datetime.now()
 current_time_milliseconds = int(current_datetime.timestamp() * 1000)
 print(current_time_milliseconds)
+
+formatted_timestamps = [current_time_milliseconds]
+# Loop through the previous days and add them to the list
+for i in range(1, DATA_COLLECTION_DAY):
+    previous_date = current_time_milliseconds - (i*24*60*60*1000)
+    formatted_timestamps.append(previous_date)
+# -> ['17.04.2024', '16.04.2024', '15.04.2024']
+
+if DATA_COLLECTION_DAY >=REMOVE_OLD_DATA_AFTER_DAYS :
+    logging.error(f"Overlapping days of collection and removal {DATA_COLLECTION_DAY} >= {REMOVE_OLD_DATA_AFTER_DAYS}")
+    sys.exit(1)
+
+
+
+# ███████╗██╗██╗     ███████╗    ██╗███╗   ██╗████████╗███████╗ ██████╗ ██████╗ ██╗████████╗██╗   ██╗     ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗
+# ██╔════╝██║██║     ██╔════╝    ██║████╗  ██║╚══██╔══╝██╔════╝██╔════╝ ██╔══██╗██║╚══██╔══╝╚██╗ ██╔╝    ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝
+# █████╗  ██║██║     █████╗      ██║██╔██╗ ██║   ██║   █████╗  ██║  ███╗██████╔╝██║   ██║    ╚████╔╝     ██║     ███████║█████╗  ██║     █████╔╝ 
+# ██╔══╝  ██║██║     ██╔══╝      ██║██║╚██╗██║   ██║   ██╔══╝  ██║   ██║██╔══██╗██║   ██║     ╚██╔╝      ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ 
+# ██║     ██║███████╗███████╗    ██║██║ ╚████║   ██║   ███████╗╚██████╔╝██║  ██║██║   ██║      ██║       ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗
+# ╚═╝     ╚═╝╚══════╝╚══════╝    ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝        ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝
+
+
+
+# newFile = False # Needed to create header in the new file
+
+# if not os.path.exists(DATA_FOLDER+"/electricity_price_calc_data.csv"):
+#     logging.debug("electricity_price_calc_data doesn't exist")
+#     with open(DATA_FOLDER+"/electricity_price_calc_data.csv", 'w') as f:
+#         logging.debug("electricity_price_calc_data file created")
+#         pass  # This creates an empty file
+
+#     logging.info(f"File does not exist, no local data available, electricity_price_calc_data.csv created successfully.")
+#     timestamp_not_in_dataframe = formatted_timestamps
+#     newFile = True #Header will be needed because new file is empty
+# else:
+#     logging.info(f"File electricity_price_calc_data already exists, skipping file creation, reading file")
+
+#     try:
+#         df_data = pd.read_csv(DATA_FOLDER+"/electricity_price_calc_data.csv", header=0, sep=";")
+#     except FileNotFoundError as e:
+#         logging.error(f"File not found: {e.filename}")
+#         sys.exit(1)
+#     except pd.errors.ParserError as e:
+#         logging.error(f"Error parsing CSV: {e}")
+#         sys.exit(1)
+#     except Exception as e:
+#         logging.error(f"An unexpected error occurred: {e}")
+#         sys.exit(1)
+    
+#     # List to store dates not in the DataFrame
+#     dates_not_in_dataframe = []
+
+#     # Check if DataFrame has debugdata
+#     if not df_data.empty:
+#         df_data['timestamp'] = pd.to_datetime(df_data['timestamp'], unit='ms').dt.tz_localize('UTC').dt.tz_convert('Europe/Paris')
+
+#         # Remove old data, see config
+#         if REMOVE_OLD_DATA:
+#             cutoff_date = datetime.now() - timedelta(days=REMOVE_OLD_DATA_AFTER_DAYS)
+#             cutoff_date = cutoff_date.astimezone(pytz.timezone('Europe/Paris'))
+#             df_data = df_data[df_data['timestamp'] > cutoff_date]
+
+
+#         # Delete all data from today
+#         df_data = df_data[df_data['timestamp'].dt.date != datetime.today().date()]
+#         df_data.reset_index(drop=True, inplace=True)
+        
+#         # Iterate through formatted dates
+#         # The idea is, that every day should have 95 entries 
+#         for timestamp in formatted_timestamps:
+#             datetime = pd.to_datetime(timestamp, unit='ms').dt.tz_localize('UTC').dt.tz_convert('Europe/Paris')
+#             logging.debug(f"timestamp in formatted_timestamps is {timestamp}")
+#             # Check if the date exists in the DataFrame
+#             if not df_data['timestamp'].isin([timestamp]).any():
+#                 logging.debug(f"{date} - No data found for date {date}, appending to dates_not_in_dataframe")
+#                 dates_not_in_dataframe.append(date)
+#             else:
+#                 logging.info(f"Using local data for {date}")
+#                 local_df = df_data[df_data['start_datetime'].dt.strftime("%d.%m.%Y") == date]
+#                 # Counting NaN values in each column
+#                 nan_counts = local_df.isna().sum()
+#                 # Counting columns with NaN values
+#                 columns_with_nan = nan_counts[nan_counts > 0].count()
+#                 logging.debug(f"{date} - Found {columns_with_nan} columns with NaN values")
+#                 # If more than half of the columns have NaN values, add this date to the dates to be downloaded
+#                 if columns_with_nan > (df_data.shape[1]/2):
+#                     logging.debug(f"{date} - More than half of the columns have NaN values, adding this date to the dates to be downloaded, removing data of {date}")
+#                     dates_not_in_dataframe.append(date)
+#                     #Remove all data of that date
+#                     df_data = df_data[df_data['start_datetime'].dt.strftime("%d.%m.%Y") != date]
+#         df_data= df_data.sort_values(by='start_datetime', ascending=True)
+#         df_data.to_csv(DATA_FOLDER+"/carbon_footprint_calc_data.csv", sep=';', header=True,index=False)                     
+#     else:
+#         # If DataFrame is empty, all dates need to be downloaded
+#         dates_not_in_dataframe = formatted_dates
+
+
+
+# # Print the dates not in the DataFrame
+# logging.debug(f"Dates not in the DataFrame: {dates_not_in_dataframe}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -79,9 +194,14 @@ for i in range (HTTP_MAX_RETRIES):
             df_Ptimestamps = pd.DataFrame(data)
             
             # Find the nearest lower timestamp to the current time (not taking the last timestamp, if timestamp looked for is in the past)
-            nearest_timestamp = df_Ptimestamps.loc[df_Ptimestamps['timestamps'] <= current_time_milliseconds, 'timestamps'].max()
-            print(f"For current time {current_time_milliseconds} ({datetime.fromtimestamp(current_time_milliseconds/1000)}) found nearest API timestamp: {nearest_timestamp} ({datetime.fromtimestamp(nearest_timestamp/1000)})\nDifference (in seconds): {int((current_time_milliseconds-nearest_timestamp)/1000)} \nDifference (in hours): {int((current_time_milliseconds-nearest_timestamp)/(1000*60))}")
-
+            nearest_timestamps = []
+            for timestamp in formatted_timestamps:
+                nearest_timestamp = df_Ptimestamps.loc[df_Ptimestamps['timestamps'] <= timestamp, 'timestamps'].max()
+                nearest_timestamps.append(nearest_timestamp)
+                logging.debug(f"For current time {timestamp} ({datetime.fromtimestamp(timestamp/1000)}) found nearest API timestamp: {nearest_timestamp} ({datetime.fromtimestamp(nearest_timestamp/1000)})\nDifference (in seconds): {int((timestamp-nearest_timestamp)/1000)} \nDifference (in hours): {int((timestamp-nearest_timestamp)/(1000*60))}")
+            # Convert array to set to remove duplicates, then convert back to array
+            nearest_timestamps = list(set(nearest_timestamps))
+            logging.debug(f"All timestamps that need to be downloaded are: {nearest_timestamps}")
         else:
             logging.error(f"Get pTimestamp - Request failed with status code: {response_pTimestamp.status_code}")
             retry = True
@@ -97,38 +217,44 @@ if retry:
 
 
 
+
 #Get Data from pTimestamp
+for nearest_timestamp in nearest_timestamps:
+    for i in range (HTTP_MAX_RETRIES):
+        try:
+            requestURL_data = f"https://www.smard.de/app/chart_data/{SMARD_FILTER_ID}/{SMARD_REGION}/{SMARD_FILTER_ID}_{SMARD_REGION}_quarterhour_{nearest_timestamp}.json"
+            response_data = requests.get(requestURL_data)
+            # Check if the request was successful (status code 200)
+            if response_data.status_code == 200:
+                retry = False
+                # Convert JSON response to Pandas Series
+                data = response_data.json()
+                series_data = data['series']
+                df_data = pd.DataFrame(series_data, columns=['timestamp', 'smard_price_E_MWh'])
+                # df_data['timestamp'] = df_data['timestamp'].dt.tz_localize('Europe/Paris')
+                # Drop rows with NaN values
+                df_data = df_data.dropna()
 
-for i in range (HTTP_MAX_RETRIES):
-    try:
-        requestURL_data = f"https://www.smard.de/app/chart_data/{SMARD_FILTER_ID}/{SMARD_REGION}/{SMARD_FILTER_ID}_{SMARD_REGION}_quarterhour_{nearest_timestamp}.json"
-        response_data = requests.get(requestURL_data)
-        # Check if the request was successful (status code 200)
-        if response_data.status_code == 200:
-            retry = False
-            # Convert JSON response to Pandas Series
-            data = response_data.json()
-            series_data = data['series']
-            df_data = pd.DataFrame(series_data, columns=['timestamp', 'smard_price_E_MWh'])
-            # Drop rows with NaN values
-            df_data = df_data.dropna()
-
-            # Add column "real_price" with value "true" for non-NaN values
-            df_data['day_Ahead'] = True
-            df_data['day_Ahead'] = df_data['day_Ahead'].astype(str)
-            print(df_data)
-        else:
+                # Add column "real_price" with value "true" for non-NaN values
+                df_data['day_Ahead'] = True
+                df_data['day_Ahead'] = df_data['day_Ahead'].astype(str)
+                logging.debug(df_data)
+            else:
+                retry = True
+                print("Error:", response_pTimestamp.status_code)
+        except requests.exceptions.RequestException as e:
+            logging.error(f"An error occurred: {e}")
             retry = True
-            print("Error:", response_pTimestamp.status_code)
-    except requests.exceptions.RequestException as e:
-        logging.error(f"An error occurred: {e}")
-        retry = True
-    if not retry:
-        break
-    time.sleep(HTTP_PAUSE_TIME)
-if retry:
-    logging.error(f"No response after {HTTP_MAX_RETRIES} requests")
-    sys.exit(1)
+        if not retry:
+            break
+        time.sleep(HTTP_PAUSE_TIME)
+    if retry:
+        logging.error(f"No response after {HTTP_MAX_RETRIES} requests")
+        sys.exit(1)
+
+    df_data.insert(0,"datetime",pd.to_datetime(df_data['timestamp'], unit='ms').dt.tz_localize('UTC').dt.tz_convert('Europe/Paris'))
+    df_data.to_csv(DATA_FOLDER+"/electricity_price_calc_data.csv",mode='a', header=True, index=False)
+
 
 
 
@@ -162,7 +288,7 @@ grundpreis_karlsruhe = 16.18 # € pro Monat
 
 for i in range (HTTP_MAX_RETRIES):
     try:
-        url = "https://www.stadtwerke-karlsruhe.de/de/pk/strom/tarifberater-strom.php?persons=4500&jahresverbrauch=4500&plz=76133+Karlsruhe&bestandskunde=0&onlinekommunikation=1"
+        url = "https://www.stadtwerke-karlsruhe.de/de/pk/strom/tarifvergleich.php?ps=ELECTRICITY&plz=76133&ort=Karlsruhe&jahresverbrauch=4500"
         response_data = requests.get(url)
         # Check if the request was successful (status code 200)
         if response_data.status_code == 200:
@@ -190,10 +316,10 @@ for i in range (HTTP_MAX_RETRIES):
                 # print(desired_table)
                 # Convert the HTML table to a Pandas DataFrame
                 df = pd.read_html(str(desired_table))[0]
-                df.drop(df.columns[2], axis=1, inplace=True)
-                # Print the DataFrame
-                # print(df)
-                value_in_cents = df.iloc[0, 1] /100 # cents pro kWh
+                df.set_index(0, inplace=True)
+                arbeitspreis_price = df.loc['Arbeitspreis', 1] # 37,52 ct/kWh
+                value_in_cents = re.sub(r'[^\d.,]', '', arbeitspreis_price) #37,52
+                value_in_cents = float(value_in_cents.replace(',', '.'))
                 print(f"Karlsruhe Stadtwerke Preis {value_in_cents} Cents pro kWh")
                 karlsruhe_price = value_in_cents / 100 *1000
                 print(f"Karlsruhe Stadtwerke Preis {karlsruhe_price} € pro MWh")
@@ -264,5 +390,5 @@ plt.legend()
 plt.savefig(DATA_FOLDER+'/electricity_price.png')
 
 
-df_data.insert(0,"timestamp_readable",pd.to_datetime(df_data['timestamp'], unit='ms'))
-df_data.to_csv(DATA_FOLDER+"/electricity_price_data.csv", sep=';', header=True, index=False)
+
+# df_data.to_csv(DATA_FOLDER+"/electricity_price_data.csv", sep=';', header=True, index=False)
